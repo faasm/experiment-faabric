4afc69d783685cddd5388d97bae5d0aa833dd329
Carlos Segarra <carlossegarragonzalez@gmail.com>
2020-12-09 15:10:43 +0000

    adding native build for lammps

--- tasks/build.py
+++ tasks/build.py
@@ -16,10 +16,10 @@ LAMMPS_DIR = join(EXPERIMENT_ROOT, "lammps")
 # https://lammps.sandia.gov/doc/Build_cmake.html
 
 
-@task
+@task(default=True)
 def build(ctx, clean=False):
     """
-    Build and install the LAMMPS Molecular Dynamics Simulator
+    Build and install the cross-compiled LAMMPS 
     """
     work_dir = join(LAMMPS_DIR, "build")
     cmake_dir = join(LAMMPS_DIR, "cmake")
@@ -34,7 +34,6 @@ def build(ctx, clean=False):
     cmake_cmd = [
         "cmake",
         "-GNinja",
-        "-DFAASM_BUILD_TYPE=wasm",
         "-DCMAKE_TOOLCHAIN_FILE={}".format(CMAKE_TOOLCHAIN_FILE),
         "-DCMAKE_BUILD_TYPE=Release",
         "-DCMAKE_INSTALL_PREFIX={}".format(install_dir),
@@ -56,3 +55,40 @@ def build(ctx, clean=False):
     if res.returncode != 0:
         raise RuntimeError("LAMMPS install failed")
 
+@task
+def native(ctx, clean=False):
+    """
+    Build and install LAMMPS natively
+    """
+    work_dir = join(LAMMPS_DIR, "build-native")
+    cmake_dir = join(LAMMPS_DIR, "cmake")
+    install_dir = join(LAMMPS_DIR, "install-native")
+
+    clean_dir(work_dir, clean)
+    clean_dir(install_dir, clean)
+
+    env_vars = copy(os.environ)
+
+    cmake_cmd = [
+        "cmake",
+        "-GNinja",
+        "-DCMAKE_C_COMPILER=/usr/bin/clang-10",
+        "-DCMAKE_CXX_COMPILER=/usr/bin/clang++-10",
+        "-DCMAKE_INSTALL_PREFIX={}".format(install_dir),
+        cmake_dir,
+    ]
+
+    cmake_str = " ".join(cmake_cmd)
+    print(cmake_str)
+
+    res = run(cmake_str, shell=True, cwd=work_dir, env=env_vars)
+    if res.returncode != 0:
+        raise RuntimeError("LAMMPS native CMake config failed")
+
+    res = run("ninja", shell=True, cwd=work_dir)
+    if res.returncode != 0:
+        raise RuntimeError("LAMMPS native build failed")
+
+    res = run("ninja install", shell=True, cwd=work_dir)
+    if res.returncode != 0:
+        raise RuntimeError("LAMMPS install failed")
