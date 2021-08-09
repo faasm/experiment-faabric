b045ebe59b64aa2d799123dea086c2a49300d9c2
Simon Shillaker <scs17@ic.ac.uk>
2021-08-09 08:25:09 +0000

    Import native lammps

--- tasks/lammps/__init__.py
+++ tasks/lammps/__init__.py
@@ -2,7 +2,8 @@ from invoke import Collection
 
 from . import container
 from . import data
+from . import native
 from . import run
 from . import wasm
 
-ns = Collection(container, data, run, wasm)
+ns = Collection(container, data, native, run, wasm)
