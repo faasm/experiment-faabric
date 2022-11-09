1192b418ea122279fe737dac12eb1b80abc1ea32
Carlos Segarra <carlos@carlossegarra.com>
2022-11-09 15:45:01 +0000

    more fixes

--- bin/create_venv.sh
+++ bin/create_venv.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-echo "Setting up venv at $(pwd)/venv"
-python3.8 -m venv venv
-source venv/bin/activate
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
+PROJ_ROOT="${THIS_DIR}/.."
+
+echo "Setting up venv at ${PROJ_ROOT}/venv"
+python3.8 -m venv ${PROJ_ROOT}/venv
+source ${PROJ_ROOT}/venv/bin/activate
+
 pip3 install -U pip
+pip_cmd install -U setuptools
 pip3 install -r requirements.txt
--- bin/workon.sh
+++ bin/workon.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-PROJ_ROOT=${THIS_DIR}/..
+set -e
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
+PROJ_ROOT="${THIS_DIR}/.."
 
 pushd ${PROJ_ROOT} >> /dev/null
 
