af351fb34771ecb30d29a207e2193c7243595384
Carlos Segarra <carlos@carlossegarra.com>
2022-12-02 12:02:04 +0000

    mpi: attempt at fixing workon

--- bin/create_venv.sh
+++ bin/create_venv.sh
@@ -6,7 +6,7 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 &
 PROJ_ROOT="${THIS_DIR}/.."
 
 echo "Setting up venv at ${PROJ_ROOT}/venv"
-python3.8 -m venv ${PROJ_ROOT}/venv
+python3.10 -m venv ${PROJ_ROOT}/venv
 source ${PROJ_ROOT}/venv/bin/activate
 
 pip3 install -U pip
